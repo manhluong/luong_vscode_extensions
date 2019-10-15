d802ad085a572abea398de74588ebf7d9e0a72d7
Luong Bui <luong_bui@discovery.com>
2019-10-15 11:01:17 +0100

    Switched to Settings Sync

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 ### List of my used VSCode extensions
 
-This is a list with the VSCode extensions that I am currently using.
+~~This is a list with the VSCode extensions that I am currently using.~~
 
-It is here only as a simple way to share the extensions across laptops.
+~~It is here only as a simple way to share the extensions across laptops.~~
 
-Command used to generate the list:
+~~Command used to generate the list:~~
 
-code --list-extensions | xargs -L 1 echo code --install-extension
+~~code --list-extensions | xargs -L 1 echo code --install-extension~~
 
-The list is then saved in a script to be run after adding `code` to the PATH.
+~~The list is then saved in a script to be run after adding `code` to the PATH.~~
+
+Switched to [Settings Sync](https://marketplace.visualstudio.com/items?itemName=Shan.code-settings-sync)
 
 ---
 
--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,23 +1,2 @@
-code --install-extension alefragnani.Bookmarks
-code --install-extension Dart-Code.dart-code
-code --install-extension Dart-Code.flutter
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension flowtype.flow-for-vscode
-code --install-extension formulahendry.code-runner
-code --install-extension ms-vscode.cpptools
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension redhat.java
-code --install-extension vscjava.vscode-java-debug
-code --install-extension vscjava.vscode-java-pack
-code --install-extension vscjava.vscode-java-test
-code --install-extension vscjava.vscode-maven
-code --install-extension pranaygp.vscode-css-peek
-code --install-extension xabikos.JavaScriptSnippets
-code --install-extension formulahendry.auto-close-tag
-code --install-extension humao.rest-client
-code --install-extension alefragnani.project-manager
-code --install-extension aaron-bond.better-comment
-code --install-extension pnp.polacode
+code --install-extension Shan.code-settings-sync
 
